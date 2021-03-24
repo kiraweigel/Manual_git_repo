783e3506a4ce97ce03594437b1e9725c00bcfc09
kiraweigel <kiraweigel4660@gmail.com>
2021-03-24 17:00:05 +0300

    123

---
+++ app.py
@@ -0,0 +1,17 @@
+git init
+
+git status
+
+git commit -m однострочный комментарий
+
+git commit многострочный комментарий в отдельном окне
+Если редактор открылся в терминале, то:
+    Писать комментарий к комиту нужно в самом верху. Чтобы выйти из редактора - жмем Esc, пишем :wq и жмем Enter.
+
+Представиться в гите
+    git config --global user.email "kiraweigel4660@gmail.com" 
+    git config --global user.name "kiraweigel"
+
+git push origin main
+
+git pull 
